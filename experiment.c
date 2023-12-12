c5e5b15c577be4d06e165fed5fa88c974482fce2
Lo Zhan Preston <zhanpreston.lo@motorolasolutions.com>
2023-12-12 15:27:58 +0800

    experiment

--- experiment.c
+++ experiment.c
@@ -11,9 +11,17 @@
  * But program still able to run
  * And the value will use the one defined in here,
  * rather than the value passed in from makefile
+ * 
+ * If you pass in '-DNUM' like that
+ * That is, no value
+ * Then, it has default value of 1 (TRUE)
 */
 
 int main()
 {
     printf("IT'S MEEEE %d\n", NUM);
+
+    #if NUM==3
+        printf("YOOOOOO\n");
+    #endif
 }
